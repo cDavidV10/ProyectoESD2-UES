397962525f885b7a756920731756e59e0e667d0b
David <cdavidvasquezr20@gmail.com>
2026-05-22 18:05:25 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/proyectoestd/ProyectoESTD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoestd;
+
+import conexion.Conexion;
+
+/**
+ *
+ * @author cdavi
+ */
+public class ProyectoESTD {
+
+    public static void main(String[] args) {
+        new Conexion().getConexion();
+    }
+}
---
+++ src/main/java/conexion/Conexion.java
@@ -0,0 +1,29 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Conexion {
+    public static Connection getConexion() {
+        Dotenv dotenv = Dotenv.load();
+        String dbHost = dotenv.get("DB_HOST");
+        String dbUser = dotenv.get("DB_USER");
+        String dbPassword = dotenv.get("DB_PASSWORD");
+
+        try {
+            Connection conexion = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+
+            if (conexion != null) {
+                System.out.println("Hay conexion");
+                return conexion;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al conectar: " + e.getMessage());
+        }
+
+        return null;
+    }
+}
