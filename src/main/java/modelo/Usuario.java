809cdc90ef25ae4f20197f18cf81705cb463d271
David <cdavidvasquezr20@gmail.com>
2026-05-25 19:05:26 -0600

    Creando modelos necesarios para el login

---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,80 @@
+package modelo;
+
+public class Usuario {
+    private int id;
+    private String username;
+    private String password;
+    private String tipo;
+    // ? Clases Relacionadas
+    private Administrador administrador;
+    private Cliente cliente;
+    private Empleado empleado;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String username, String password, String tipo) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Administrador getAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(Administrador administrador) {
+        this.administrador = administrador;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+    
+    
+}
