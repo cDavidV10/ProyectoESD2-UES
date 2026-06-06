1ea16243d552575d2c7988f421f9b1ec32c538ff
man_uh <enmanuelvsqz01@gmail.com>
2026-06-05 19:10:50 -0600

    Vista corregida y SQLS en mejor formato para los DAOs de Lectura y Factura

--- src/main/java/dao/FacturaDAO.java
+++ src/main/java/dao/FacturaDAO.java
@@ -9,7 +9,10 @@ import modelo.Factura;
 
 public class FacturaDAO implements IFacturaDAO {
 
-    private static final String INSERT = "INSERT INTO public.factura(fecha_limite, mora, monto_consumo, monto_servicio, monto_neto, monto_total, id_lectura, id_empleado) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+    private static final String INSERT = """
+                                INSERT INTO public.factura(fecha_limite, mora, monto_consumo, monto_servicio, monto_neto, monto_total, id_lectura, id_empleado)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                                """;
 
     @Override
     public boolean guardar(Factura factura) throws Exception {
@@ -33,7 +36,7 @@ public class FacturaDAO implements IFacturaDAO {
             if (filasAfectadas > 0) {
                 guardado = true;
             }
-            
+
             conn.commit();
 
         } catch (Exception ex) {
@@ -46,7 +49,7 @@ public class FacturaDAO implements IFacturaDAO {
                 conn.close();
             }
         }
-        
+
         return guardado;
     }
 }
